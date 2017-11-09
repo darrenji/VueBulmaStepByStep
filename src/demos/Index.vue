e0e25ce4a88cd22ac6a0a67f8b95cf11c2acc587
darren <764190362@qq.com>
2017-11-09 21:09:10 +0800

    右侧首页，完成布局

--- src/demos/Index.vue
+++ src/demos/Index.vue
@@ -17,7 +17,99 @@
             </ul>
         </div>
         <div class="tabs-content-container">
-            <div>content</div>
+            <!--简介-->
+            <div class="fade" v-show="index===0">
+                <div class="tile is-ancestor">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification is-normal">
+                            <div class="content">
+                                <p class="title">这里是标题</p>
+                                <p class="subtitle">这里是副标题</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, quam nam voluptatem ut repellendus quo harum dicta voluptas, quod sit sed hic suscipit corporis blanditiis! Repudiandae accusamus unde atque, optio.</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates sequi atque id unde vitae dicta quidem sunt libero, reprehenderit iste eveniet accusantium. Dolore, fugit in ut ipsa, rerum neque voluptate?</p>
+                                <div class="content">
+                                    
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+            
+            <!--布局-->
+            <div class="fade" v-show="index===1">
+               <!--最上面的一块tile, 层次结构：tile is-ancestor, tile is-parent, tile is-child, content, title+subtitle-->
+               <!--横向占满-->
+                <div class="tile is-ancestor">
+                  <div class="tile is-parent">
+                    <article class="tile is-child notification is-normal">
+                      <div class="content">
+                        <p class="title">Flex Layout</p>
+                        <p class="subtitle">弹性盒内滚动布局</p>
+                        <p>支持所有新一代浏览器，为获得最优的效果优先推荐使用最新版Chrome，本项目设计为PC端的管理后台使用。</p>
+                        <div class="content">
+                          <!-- Content -->
+                        </div>
+                      </div>
+                    </article>
+                  </div>
+                </div>
+                
+                <!--横向占满-->
+                <div class="tile is-ancestor">
+                   <!--垂直排列，横向占了8格-->
+                    <div class="tile is-vertical is-8">
+                       <!--横向占了8格-->
+                        <div class="tile">
+                            <!--横向占了4格-->
+                            <div class="tile is-parent is-vertical">
+                               <!--上面-->
+                                <article class="tile is-child notification is-primary">
+                                    <p class="title">Verticle</p>
+                                    <p class="subtitle">Top tile</p>
+                                </article>
+                                <!--下面-->
+                                <article class="tile is-child notification is-warning">
+                                    <p class="title">...tiles</p>
+                                    <p class="subtitle">Bottom tile</p>
+                                </article>
+                            </div>
+                            <!--横向占了4格-->
+                            <div class="tile is-parent">
+                                <article class="tile is-child notification is-info">
+                                    <p class="title">Middle tile</p>
+                                    <p class="subtitle">With an image</p>
+                                    <figure class="image is-4by3">
+                                        <img src="http://placehold.it/640x480" alt="">
+                                    </figure>
+                                </article>
+                            </div>
+                        </div>
+                        <!--横向占了8格-->
+                        <div class="tile is-parent">
+                            <article class="tile is-child notification is-danger">
+                                <p class="title">Wide title</p>
+                                <p class="subtitle">Aligned with rhe right tile</p>
+                                <div class="content">
+                                    
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                    <!--填充横向剩下的4格-->
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification is-success">
+                            <div class="content">
+                                <p class="title">Tall tile</p>
+                                <p class="subtitle">With even more content</p>
+                                <div class="content">
+                                    
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
         </div>
     </xcontent>
 </template>
